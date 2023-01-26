209daf941a555ff46f0778e8ee2b0dfd3678b64b
Kalos99 <calogerocorsello1999@gmail.com>
2023-01-26 15:54:47 +0100

    fix

--- cibandoApp/src/app/components/recipes/recipes.component.ts
+++ cibandoApp/src/app/components/recipes/recipes.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { RecipeService } from './../../services/recipe.service';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss']
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit{
+
+  ricette: Recipe[];
+
+  constructor(private recipeService: RecipeService){ }
+
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe({
+      next: (ricette) => {
+        this.ricette = ricette;
+      },
+      error: (errore) => {
+        console.log(errore);
+      }
+    })
+  }
 
 }
